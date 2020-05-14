6d0926710a45131f410049eb0e84da8333ec368f
Alex Rodriguez <alexrodriguez0719@gmail.com>
2020-05-14 15:05:53 -0500

    5/14/20 - updated pom, added test.html, added new tests

--- src/test/java/edu/depaul/email/EmailFinderTest.java
+++ src/test/java/edu/depaul/email/EmailFinderTest.java
@@ -1,29 +1,20 @@
 
 package edu.depaul.email;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import java.util.Set;
 
 public class EmailFinderTest {
 
-    @Test
-    @DisplayName("A test for EmailFinder")
-    void testEmailFinder(){
-        String[] url = {"https://cdm.depaul.edu"};
-        EmailFinder finder = new EmailFinder();
-        finder.run(url);
-    }
 
     @Test
     @DisplayName("Constructor should throw an exception when an incorrect url is given")
@@ -38,7 +29,8 @@ public class EmailFinderTest {
         String html = "<html><a href='/some/other/file.html'>my link</a></body></html>";
         Document doc = Jsoup.parse(html);
         PageParser parser = new PageParser();
-        Set<String> links = parser.findLinks(doc);
+        parser.findLinks(doc);
+        assertEquals(null,doc.getElementById("my link"));
     }
 
 }
